1a50b53ddc3453eae07d3a6967107c941a16d731
MaksymBora <maxboraod@gmail.com>
2023-05-23 23:49:48 +0200

    add 1st gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,66 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+//     description: 'Hokkaido Flower',
+//   },
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}" >
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+</li>`
+    )
+    .join('');
+}
+
+gallery.addEventListener('click', onClick);
+
+function onClick(e) {
+  e.preventDefault();
+
+  const { target } = e;
+
+  if (!target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const imageSource = target.dataset.source;
+
+  zoomPicture(imageSource);
+}
+
+function zoomPicture(source) {
+  const instance = basicLightbox.create(`
+    <img src="${source}" width="1280" height="720">
+`);
+
+  instance.show();
+
+  const onEscKeyPress = function (e) {
+    const ESC_KEY_CODE = 'Escape';
+    if (e.code === ESC_KEY_CODE) {
+      instance.close();
+      window.removeEventListener('keydown', onEscKeyPress);
+    }
+  };
+  window.addEventListener('keydown', onEscKeyPress);
+}
